ad02fe56d132af00a629abc2ab4992c16a281870
Ben Slabbert <ben@hex.co.za>
2017-12-18 13:29:18 +0200

    wiring up employee list

--- client/main.js
+++ client/main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import EmployeeList from './components/EmployeeList';
+
 const App = () => {
 
     return (
         <div>
-            hey
+            <EmployeeList/>
         </div>
     );
 };
